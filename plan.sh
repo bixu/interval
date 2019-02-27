7b257e8eb2e9ae88bf723258c59bc842dde68a5f
Blake Irvin <blake.irvin@gmail.com>
2019-02-27 17:39:39 +0100

    commit initial working version

---
+++ plan.sh
@@ -0,0 +1,18 @@
+pkg_name=interval
+pkg_origin=bixu
+pkg_version=0.1.0
+pkg_description="Do a thing roughly every N seconds."
+pkg_license=('Apache 2')
+pkg_maintainer="Blake Irvin <blakeirvin@me.com>"
+pkg_deps=(
+  'core/bash'
+  'core/coreutils'
+)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
